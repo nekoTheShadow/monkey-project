4698b580dd3b545c7d5b70b5afb6df49f4cbcb75
hajime nakamura <h.nakamura0903@gmail.com>
2020-01-05 20:48:59 +0900

    A.5.4 マクロを展開する

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -509,3 +509,62 @@ func addMacro(stmt ast.Statement, env *object.Environment) {
 	}
 	env.Set(letStatement.Name.Value, macro)
 }
+
+func ExpandMacros(program ast.Node, env *object.Environment) ast.Node {
+	return ast.Modify(program, func(node ast.Node) ast.Node {
+		callExpresion, ok := node.(*ast.CallExpression)
+		if !ok {
+			return node
+		}
+
+		macro, ok := isMacroCall(callExpresion, env)
+		if !ok {
+			return node
+		}
+
+		args := quoteArgs(callExpresion)
+		evalEnv := expandMacroEnv(macro, args)
+		evaluated := Eval(macro.Body, evalEnv)
+
+		quote, ok := evaluated.(*object.Quote)
+		if !ok {
+			panic("we only support returning AST-nodes from macros")
+		}
+		return quote.Node
+	})
+}
+
+func isMacroCall(exp *ast.CallExpression, env *object.Environment) (*object.Macro, bool) {
+	identifier, ok := exp.Function.(*ast.Identifier)
+	if !ok {
+		return nil, false
+	}
+
+	obj, ok := env.Get(identifier.Value)
+	if !ok {
+		return nil, false
+	}
+
+	macro, ok := obj.(*object.Macro)
+	if !ok {
+		return nil, false
+	}
+
+	return macro, true
+}
+
+func quoteArgs(exp *ast.CallExpression) []*object.Quote {
+	args := []*object.Quote{}
+	for _, a := range exp.Arguments {
+		args = append(args, &object.Quote{Node: a})
+	}
+	return args
+}
+
+func expandMacroEnv(macro *object.Macro, args []*object.Quote) *object.Environment {
+	extended := object.NewEnclosedEnvironment(macro.Env)
+	for paramIdx, param := range macro.Parameters {
+		extended.Set(param.Value, args[paramIdx])
+	}
+	return extended
+}
